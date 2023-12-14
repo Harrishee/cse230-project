5ae3e758cbf26edcefb68ff778ce09913c9495ab
adghadmin <yyh17860738857@hotmail.com>
2023-12-13 23:58:44 -0800

    resolve conflicts

--- src/Maps.hs
+++ src/Maps.hs
@@ -16,41 +16,6 @@ data Item = Item { itemType :: ItemType, itemCoord :: Coord } deriving (Eq, Show
 
 type LevelMaps = [([Coord], [Item])]
 
-<<<<<<< HEAD
-predefinedItems :: Int -> Seq Item
-predefinedItems levelId = case levelId of
-  1 -> S.fromList [
-    Item Bronze (V2 1 3), Item Silver (V2 2 5), Item Gold (V2 3 7),
-    Item Bronze (V2 4 9), Item Silver (V2 5 11), Item Gold (V2 6 13),
-    Item Bronze (V2 7 2), Item Pickable (V2 8 4), Item Gold (V2 9 6),
-    Item Bronze (V2 10 8), Item Bomb (V2 11 10), Item Gold (V2 12 12),
-    Item Silver (V2 13 14)]
-  2 -> S.fromList [
-    Item Bronze (V2 1 3), Item Silver (V2 2 5), Item Gold (V2 3 7),
-    Item Bronze (V2 4 9), Item Silver (V2 5 11), Item Gold (V2 6 13),
-    Item Bronze (V2 7 2), Item Pickable (V2 8 4), Item Gold (V2 9 6),
-    Item Bronze (V2 10 8), Item Bomb (V2 11 10), Item Gold (V2 12 12),
-    Item Silver (V2 13 14)]
-  3 -> S.fromList [
-    Item Bronze (V2 1 3), Item Silver (V2 2 5), Item Gold (V2 3 7),
-    Item Bronze (V2 4 9), Item Silver (V2 5 11), Item Gold (V2 6 13),
-    Item Bronze (V2 7 2), Item Silver (V2 8 4), Item Gold (V2 9 6),
-    Item Bronze (V2 10 8), Item Silver (V2 11 10), Item Gold (V2 12 12),
-    Item Silver (V2 13 14)]
-  4 -> S.fromList [
-    Item Bronze (V2 1 3), Item Silver (V2 2 5), Item Gold (V2 3 7),
-    Item Bronze (V2 4 9), Item Silver (V2 5 11), Item Gold (V2 6 13),
-    Item Bronze (V2 7 2), Item Silver (V2 8 4), Item Gold (V2 9 6),
-    Item Bronze (V2 10 8), Item Silver (V2 11 10), Item Gold (V2 12 12),
-    Item Silver (V2 13 14)]
-  5 -> S.fromList [
-    Item Bronze (V2 1 3), Item Silver (V2 2 5), Item Gold (V2 3 7),
-    Item Bronze (V2 4 9), Item Silver (V2 5 11), Item Gold (V2 6 13),
-    Item Bronze (V2 7 2), Item Silver (V2 8 4), Item Gold (V2 9 6),
-    Item Bronze (V2 10 8), Item Silver (V2 11 10), Item Gold (V2 12 12),
-    Item Silver (V2 13 14)]
-  _ -> S.empty
-=======
 predefinedMaps :: [LevelMaps]
 predefinedMaps = 
     [
@@ -104,4 +69,3 @@ predefinedWalls levelId = fmap fst $ getRandomMap levelId
 
 predefinedItems :: Int -> IO (Seq Item)
 predefinedItems levelId = fmap snd $ getRandomMap levelId
->>>>>>> add_maps
